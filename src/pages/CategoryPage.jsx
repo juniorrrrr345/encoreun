d78d420dafb54995400f5afe407e56a402139f38
juniorrrrr345 <Sousgrosj1@gmail.com>
2025-07-17 05:35:08 +0200

    Create CategoryPage.jsx

---
+++ src/pages/CategoryPage.jsx
@@ -0,0 +1,13 @@
+// src/pages/CategoryPage.jsx
+import React from 'react';
+
+const CategoryPage = () => {
+  return (
+    <div>
+      <h1>Catégories</h1>
+      {/* Ton contenu ici */}
+    </div>
+  );
+};
+
+export default CategoryPage;
